5f09ee1f3b83a46252f52752341776ddc63d8995
Lucas Barbosa Silva <blucas.oficial@gmail.com>
2024-09-21 16:36:08 -0300

    conexao com SQL

--- src/main/java/com/lucas/forum/controller/TopicoController.java
+++ src/main/java/com/lucas/forum/controller/TopicoController.java
@@ -3,21 +3,20 @@ package com.lucas.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.lucas.forum.controller.dto.TopicoDTO;
 import com.lucas.forum.modelo.Curso;
 import com.lucas.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicoController {
 	
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
-		Topico topico = new Topico("Duvida", "Duvida sobre o Spring", new Curso("Springboot", "Programação"));
+	public List<TopicoDTO> lista(){
+		Topico topico = new Topico("Dúvida", "Dúvida sobre o Spring", new Curso("Springboot", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
 	}
 }
